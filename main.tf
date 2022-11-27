474fdff9d14d8ad582cc418aac159781b703f4eb
Amaldeep P J <amaldeeppjcoc1@gmail.com>
2022-11-27 23:23:57 +0530

    sg

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "this" {
     key_name = var.key_name
     subnet_id = var.subnet_id
     user_data = var.user_data
-    security_groups = var.security_groups
+    vpc_security_group_ids = var.security_groups
 
     tags = var.tags
   
