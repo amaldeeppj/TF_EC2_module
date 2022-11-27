db2be3b9fb75ee673a22d6e8b2212c093b3d3fa7
Amaldeep P J <amaldeeppjcoc1@gmail.com>
2022-11-27 22:25:04 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "ami_id" {
+    type = string
+    description = "ID of the AMI to deploy ec2 instance"
+
+    validation {
+      condition = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
+      error_message = "Please enter a valid AMI ID starting with 'ami-'"
+    }
+  
+}
+
+variable "key_name" {
+    type = string
+    description = "key name"
+  
+}
+
+
+variable "tags" {
+    type = map(string)
+    description = "tags for EC2"
+}
+
+variable "user_data" {
+    type = string
+    default = << EOF
+        #!/bin/bash
+        echo "no userdata" > /var/user_data
+        EOF
+}
